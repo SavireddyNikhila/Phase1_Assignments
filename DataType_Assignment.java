a98a50b5bb27b82ab0a01467b70b4e71bc3d3c76
SavireddyNikhila <savireddynikhila2001@gmail.com>
2022-05-01 00:22:43 +0530

    first commit

---
+++ DataType_Assignment.java
@@ -0,0 +1,34 @@
+package phase1.core.programs;
+
+public class DataType_Assignment {
+
+	public static void main(String[] args) {
+		System.out.println("Task-1");
+		char ch='J';
+		System.out.println(ch); 
+		int a=ch;
+		System.out.println(a);
+		float b=a;
+		System.out.println(b);
+		
+		System.out.println("Task-2");
+		float h=456.9822f;
+		System.out.println(h);
+		long L=(int)h;
+		System.out.println(L);
+		char c=(char)L;
+		System.out.println(c);
+		
+		System.out.println("Task-3");
+		double d=756654.876786876876;
+		System.out.println(d);
+		int i=(int)d;
+		System.out.println(i);
+		long l=i;
+		System.out.println(l);
+		byte B=(byte)l;
+		System.out.println(B);
+
+	}
+
+}
