3717e6718dc712e1369aeaf09a3246d1127d2449
SavireddyNikhila <savireddynikhila2001@gmail.com>
2022-05-01 00:24:19 +0530

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,33 @@
+package phase1.core.programs;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("We are in wipro training");
+
+		int age = 45;
+		long k = age;
+		double d = age;
+		System.out.println("age is :" + d);
+
+		long distance = 1234654363777873L;
+		int i = (int) distance;
+		System.out.println("distance is :" + i);
+
+		char gender = 'F';
+		int r = gender;
+		System.out.println("Gender is :" + r);
+
+		float percentage = 98.7465f;
+		int f = (int) percentage;
+		System.out.println("Float value is:" + f);
+
+		double pi = 3.247685432098;
+		System.out.println("PI value is :" + pi);
+
+		boolean status = true;
+		System.out.println("Status is :" + status);
+
+	}
+
+}
